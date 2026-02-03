e73970c51865d93576d13164ada4069c684661ef
sidde <siddeshshirote30052006@gmail.com>
2026-02-03 23:55:26 +0530

    compeleted authentication

--- backend/9.6+Secrets+Project/index.js
+++ backend/9.6+Secrets+Project/index.js
@@ -4,6 +4,7 @@ import pg from "pg";
 import bcrypt from "bcrypt";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
 import env from "dotenv";
 
@@ -55,14 +56,50 @@ app.get("/logout", (req, res) => {
     });
 });
 
-app.get("/secrets", (req, res) => {
-    // console.log(req.user);
+app.get("/secrets", async (req, res) => {
     if (req.isAuthenticated()) {
-        res.render("secrets.ejs");
+        try {
+            const result = await db.query('SELECT secret FROM users WHERE email = $1', [req.user.email]);
+            const secret = result.rows[0].secret;
+            if (secret) {
+                res.render("secrets.ejs", { secret: secret });
+            } else {
+                res.render("secrets.ejs", { secret: "bad things will happen in future where you are in black hole " });
+            }
+        } catch (err) {
+            console.log(err);
+        }
+
+        //TODO: Update this to pull in the user secret to render in secrets.ejs
     } else {
         res.redirect("/login");
     }
 });
+
+app.get("/submit", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("submit.ejs");
+    } else {
+        res.redirect("/login");
+    }
+});
+//TODO: Add a get route for the submit button
+//Think about how the logic should work with authentication.
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"],
+    })
+);
+
+app.get(
+    "/auth/google/secrets",
+    passport.authenticate("google", {
+        successRedirect: "/secrets",
+        failureRedirect: "/login",
+    })
+);
 
 app.post(
     "/login",
@@ -105,42 +142,53 @@ app.post("/register", async (req, res) => {
     }
 });
 
-passport.use(
-    new Strategy(async function verify(username, password, cb) {
-        try {
-            const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
-                username,
-            ]);
-            if (result.rows.length > 0) {
-                const user = result.rows[0];
-                const storedHashedPassword = user.password;
-                bcrypt.compare(password, storedHashedPassword, (err, valid) => {
-                    if (err) {
-                        //Error with password check
-                        console.error("Error comparing passwords:", err);
-                        return cb(err);
-                    } else {
-                        if (valid) {
-                            //Passed password check
-                            return cb(null, user);
-                        } else {
-                            //Did not pass password check
-                            return cb(null, false);
-                        }
-                    }
-                });
-            } else {
-                return cb("User not found");
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    })
-);
+//TODO: Create the post route for submit.
+//Handle the submitted data and add it to the database
+app.post("/submit", async (req, res) => {
+    const secret = req.body.secret;
+    try {
+        await db.query("Update users SET secret = $1 WHERE email = $2", [secret, req.user.email]);
+        res.redirect("/secrets");
 
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+passport.use(
+    "google",
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/google/secrets",
+            userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+        },
+        async (accessToken, refreshToken, profile, cb) => {
+            try {
+                console.log(profile);
+                const result = await db.query("SELECT * FROM users WHERE email = $1", [
+                    profile.email,
+                ]);
+                if (result.rows.length === 0) {
+                    const newUser = await db.query(
+                        "INSERT INTO users (email, password) VALUES ($1, $2)",
+                        [profile.email, "google"]
+                    );
+                    return cb(null, newUser.rows[0]);
+                } else {
+                    return cb(null, result.rows[0]);
+                }
+            } catch (err) {
+                return cb(err);
+            }
+        }
+    )
+);
 passport.serializeUser((user, cb) => {
     cb(null, user);
 });
+
 passport.deserializeUser((user, cb) => {
     cb(null, user);
 });
