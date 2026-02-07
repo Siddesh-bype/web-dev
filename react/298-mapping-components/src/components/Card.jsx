08ba0c109c1db7c0175c7267d043ac2e92ba5235
sidde <siddeshshirote30052006@gmail.com>
2026-02-08 01:14:57 +0530

    day 6 react -299- completed

--- react/298-mapping-components/src/components/Card.jsx
+++ react/298-mapping-components/src/components/Card.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import Avatar from "./Avatar";
 import Detail from "./Detail";
+
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <Avatar img={props.img}/>
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
-        <Detail 
-        detailinfo={props.tel}/>
-        <Detail 
-        detailinfo={props.email}/>
+        <Detail detailInfo={props.tel} />
+        <Detail detailInfo={props.email} />
       </div>
     </div>
   );
 }
 
 export default Card;
- 
