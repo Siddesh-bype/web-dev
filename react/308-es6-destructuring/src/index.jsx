8ab1099200bef4e9223f4a59617b69354ffbca5c
sidde <siddeshshirote30052006@gmail.com>
2026-02-10 23:14:35 +0530

    day 9 react-308 completed

--- react/308-es6-destructuring/src/index.jsx
+++ react/308-es6-destructuring/src/index.jsx
@@ -1,26 +1,33 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import {createRoot} from "react-dom/client";
+import cars from "./practice";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+const [honda, tesla]=cars;
+const {speedStats:{topSpeed: hondaTopSpeed}}=honda;
+const {coloursByPopularity:[hondaTopColour]}=honda;
+
+const {speedStats:{topSpeed: teslaTopSpeed}}=tesla;
+const {coloursByPopularity:[teslaTopColour]}=tesla;
+
+createRoot(document.getElementById("root")).render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>
+);
 
 // If you're running this locally in VS Code use the commands:
 // npm install
