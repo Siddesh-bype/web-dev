adddf7538a72a832aa7a6959d5008c90f5a626e4
sidde <siddeshshirote30052006@gmail.com>
2026-02-12 00:22:22 +0530

    day 10 react-311 completed

--- react/310-event-handling-in-react/src/components/App.jsx
+++ react/310-event-handling-in-react/src/components/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+const [headingText, setHeadingText] = useState("hello");
+const [bg, setbg] = useState(false);
+
+function handleClick() {
+  setHeadingText("Submitted");
+}
+
+function onMouseOver() {
+  setbg(true);
+}
+
+function onMouseOut() {
+  setbg(false);
+}
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: bg ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={onMouseOver}
+        onMouseOut={onMouseOut}>
+        Submit
+      </button>
     </div>
   );
 }
--- react/311-react-forms/src/components/App.jsx
+++ react/311-react-forms/src/components/App.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("HELLO");
+  const [OnMouse, setOnMouse] = useState(false);
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+  function handleClick() {
+    setHeadingText("Hello " + name);
+  }
+  function handleMouseOver() {
+    setOnMouse(true);
+  }
+  function handleMouseOut() {
+    setOnMouse(false);
+  }
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{headingText}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button
+        style={{backgroundColor: OnMouse? "black":"white" }}
+        onClick={handleClick}
+        onMouseOut={handleMouseOut}
+        onMouseOver={handleMouseOver}>
+        Submit
+      </button>
     </div>
   );
 }
