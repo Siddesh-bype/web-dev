dea21ddbc2128873567b0cf6658bcf2bb6f1086a
sidde <siddeshshirote30052006@gmail.com>
2026-02-20 22:20:52 +0530

    day 12 react-start

--- react/314-changing-complex-state-practice/src/index.jsx
+++ react/314-changing-complex-state-practice/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById("root")).render(<App />);
 
 //CHALLENGE: Make the code in App.jsx work.
 //The final app should have a single contact
